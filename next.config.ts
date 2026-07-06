f4519cc9620d9ac9fbbd1afdcf51d48d162c702d
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-06 18:31:57 +0300

    Configure Next export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  ...(isGithubPages
+    ? {
+        basePath: "/personalportfolio",
+        assetPrefix: "/personalportfolio/",
+      }
+    : {}),
 };
 
 export default nextConfig;
